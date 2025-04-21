71584def87ffbec521e57dfcba226b63797e18c2
KishonShrill <crystalbluew@gmail.com>
2025-04-21 21:36:51 +0800

    Fixed the Link by adding Suspense fallback

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { scan } from 'react-scan'
 // --- React Query Setup ---
 scan({enabled: import.meta.env.VITE_SCAN === "true",});
 
-import React, { useState, lazy } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
@@ -11,39 +11,41 @@ import Cookies from "universal-cookie";
 const cookies = new Cookies();
 import './styles/App.scss'
 
-import Header from "./components/Header.jsx";
-import Homepage from './pages/Homepage.jsx';
+import Header from "./components/Header.jsx"
+import Homepage from "./pages/Homepage.jsx"
+const GroceryPage = lazy(() => import("./pages/GroceryPage.jsx"))
+const ReceiptPage = lazy(() => import("./pages/ReceiptPage.jsx"))
 
-import Login from './components/Login.jsx'
-import AuthComponent from './pages/AuthComponent.jsx';
+const Login = lazy(() => import("./components/Login.jsx"))
+const AuthComponent = lazy(() => import("./pages/AuthComponent.jsx"))
+const CreateItem = lazy(() => import("./components/CreateItem.jsx"))
+const NoPage = lazy(() => import("./pages/NoPage.jsx"))
 
-const GroceryPage = lazy(() => import("./pages/GroceryPage.jsx"));
-const NoPage = lazy(() => import("./components/NoPage.jsx"));
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
   const token = cookies.get("TOKEN");
-
-  function addToCart(product) {
-    setCartItems([...cartItems, product]);
-  }
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Header token={token} />
-          <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route exact path="/groceries" element={<GroceryPage/>} />
-            <Route exact path="/authenticate" element={<Login />} />
-            <Route path="/dev-mode" element={token ? <AuthComponent /> : <Navigate to="/" replace />}/>
-            <Route path="*" element={<NoPage />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/groceries" element={<GroceryPage/>} />
+              <Route exact path="/receipt" element={<ReceiptPage />} />
+              
+              <Route exact path="/authenticate" element={<Login />} />
+              <Route path="/dev-mode" element={token ? <AuthComponent /> : <Navigate to="/" replace />}/>
+              <Route path='/groceries/add-item' element={<CreateItem />}/>
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
-        {/* <ReactQueryDevtools initialIsOpem={false} position='bottom-right'/> */}
+        <ReactQueryDevtools initialIsOpem={false} position='bottom-right'/>
       </QueryClientProvider>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import '../styles/main-header.scss'
 
 export default function Header({ token }) {
+  const location = useLocation(); // ✅ also needs to be here, not in render logic
+  console.log({location})
+
   return (
     <>
       <header className="header">
-        <a href="/" className="header__name" style={{fontWeight: 700}}>Budget Buddy</a>
+        <Link to="/" className="header__name" style={{fontWeight: 700}}>Budget Buddy</Link>
         <nav className="header__nav">
           <ul className='header__nav-links'>
             <li><Link to="/">Home</Link></li>
@@ -14,11 +18,11 @@ export default function Header({ token }) {
           </ul>
           <div className="header__nav-login">
             {!token ? (
-                <a className='login__devmode' href="/authenticate">Login</a>
+                <Link className='login__devmode' href="/authenticate">Login</Link>
               ) : (
-                <a className='login__devmode' href="/dev-mode">Dev Mode</a>
+                <Link className='login__devmode' href="/dev-mode">Dev Mode</Link>
             )}
-            <Link to="/cart" >Cart</Link>
+            <Link to="/receipt">Cart</Link>
           </div>
         </nav>
       </header>
